654b8c7dd89dfb64d6588e0bacf8bdc1533c2d4f
hienthanh503 <113532058+hienthanh503@users.noreply.github.com>
2022-10-17 14:23:59 +0700

    Add files via upload

---
+++ displayInformation.py
@@ -0,0 +1,10 @@
+Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("họ và tên: Tô Hiến Thành")
+họ và tên: Tô Hiến Thành
+print("Ngày sinh: 05.03.2022")
+Ngày sinh: 05.03.2022
+print("Địa chỉ: 74 Ng Văn Cừ, phường Ng Cư Trinh, quận 1")
+Địa chỉ: 74 Ng Văn Cừ, phường Ng Cư Trinh, quận 1
+print("Mục đích học python: biết thêm kiến thức và vận dụng vào nghề nghiệp sau này")
+Mục đích học python: biết thêm kiến thức và vận dụng vào nghề nghiệp sau này
